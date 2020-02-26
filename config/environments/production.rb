37ebdb9f6c048d3a43b38efb33fa1c7eb900e072
Bryan Stewart <bstewart@lolconcepts.com>
2020-02-26 11:31:39 -0500

    fix mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  #config.action_mailer.default_url_options = { :host => 'lolconcepts.com' }
+
+  config.action_mailer.smtp_settings = {
+    :address              => "mail.lolconcepts.com",
+    :port                 => 587,
+    :user_name            => ENV['MAIL_USER'],
+    :password             => ENV['MAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true,
+    :openssl_verify_mode  => "none"
+
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
