6f2c1bd4f53147bd67cc090acba1fd7e8e503813
Bryan Stewart <bryan.stewart@Bryans-MacBook-Pro-2.local>
2020-09-24 14:46:37 -0400

    added delete to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :chores, :dependent => :destroy end
+  has_many :chores, :dependent => :destroy
 
    mount_uploader :avatar, AvatarUploader
 
